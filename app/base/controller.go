bdba95cfd719ca8783f75267475c528c812d8aa3
Daewu <daewu.bintara1996@gmail.com>
2022-08-09 06:52:21 +0700

    add controller request get value method

--- app/base/controller.go
+++ app/base/controller.go
@@ -1,7 +1,31 @@
 package base
 
+import "github.com/gin-gonic/gin"
+
 type Controller struct {}
 
-func NewController() Controller {
-	return Controller{}
+func (controller Controller) Request(c *gin.Context) request {
+	return request{c: c}
+}
+
+type request struct {
+	c *gin.Context
+}
+
+// Value is function to get value by key
+func(req request) Value(key string) string {
+	switch req.c.Request.Method {
+	case "GET":
+		return req.c.Query(key)
+	case "POST":
+		dataPost := req.c.PostForm(key)
+		if dataPost == "" {
+			var myMap = make(map[string]string)
+			req.c.BindJSON(&myMap)
+			return myMap[key]
+		}
+		return dataPost
+	default:
+		return ""
+	}
 }
