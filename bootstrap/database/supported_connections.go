bcf750d55c659982c2159e4298a773ee9a729f44
Daewu <daewu.bintara1996@gmail.com>
2023-05-20 23:46:50 +0700

    config database connection ready

---
+++ bootstrap/database/supported_connections.go
@@ -0,0 +1,9 @@
+package database
+
+// []string constanta of supported connection
+const MYSQL = "mysql"
+
+// / List of supported database connections
+var supportedConnections []string = []string{
+	MYSQL,
+}
---
+++ bootstrap/database/test/database_connection_test.go
@@ -0,0 +1,29 @@
+package test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"go_base_project/bootstrap/database"
+	"go_base_project/provider"
+	"gorm.io/gorm"
+	"testing"
+)
+
+type Member struct {
+	gorm.Model
+	Name          string
+	Email         string
+	Level         string
+	Status        string
+	Settings      string
+	MemberLevelId int
+	Verified      int
+	ReferalId     int
+}
+
+func TestDatabaseConnection(t *testing.T) {
+	provider.LoadEnv()
+	mm := Member{}
+	database.Open().Main().Where("id = ?", 2).First(&mm)
+
+	assert.IsTypef(t, Member{}, mm, "Okee")
+}
