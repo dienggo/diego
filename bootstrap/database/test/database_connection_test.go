1e356c21b21c19f4147412adbcff0ad8119910c8
Daewu <daewu.bintara1996@gmail.com>
2023-05-21 14:17:58 +0700

    update test db connection

--- bootstrap/database/test/database_connection_test.go
+++ bootstrap/database/test/database_connection_test.go
@@ -3,25 +3,12 @@ package test
 import (
 	"go_base_project/bootstrap/database"
 	"go_base_project/provider"
-	"gorm.io/gorm"
 	"testing"
 )
-
-type Member struct {
-	gorm.Model
-	Name          string
-	Email         string
-	Level         string
-	Status        string
-	Settings      string
-	MemberLevelId int
-	Verified      int
-	ReferalId     int
-}
 
 func TestDatabaseConnection(t *testing.T) {
 	provider.LoadEnv()
 	defer database.Close()
 	db := database.Open()
-	println("database is not null", db.Main() != nil)
+	println("database is not nil", db.Main() != nil)
 }
