101bdf3791d01de63dbaccc2601db992f6c465bf
Daewu <daewu.bintara1996@gmail.com>
2023-08-07 23:05:24 +0700

    update version readme

--- cmd/diego/main.go
+++ cmd/diego/main.go
@@ -15,8 +15,8 @@ const (
 	name        = "diego"
 	usage       = "diego"
 	greetings   = "Hi, welcome to diego framework!"
-	version     = "v1.2.2"
-	nextVersion = "v1.2.3"
+	version     = "v1.2.3"
+	nextVersion = "v1.2.4"
 )
 
 type ICommand interface {
