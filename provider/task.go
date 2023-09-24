e4cb872875fed8d5e741ffb57a38243a99c8f7a4
Daewu <daewu.bintara1996@gmail.com>
2023-09-24 21:20:23 +0700

    add strategy shutting down background task

--- provider/task.go
+++ provider/task.go
@@ -2,8 +2,12 @@ package provider
 
 import (
 	"context"
+	"fmt"
 	"github.com/dienggo/diego/app/background"
 	"github.com/dienggo/diego/pkg/task"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 // Don't remove or edit manually all comments on this method
@@ -17,7 +21,19 @@ func tasks() []task.ITask {
 type appTask struct{}
 
 func (t appTask) Provide() {
+	go runTask()
+}
+
+// runTask : run task on other thread
+func runTask() {
+	ctx := context.Background()
 	for _, iTask := range tasks() {
-		go func() { task.Runner(iTask, context.Background(), false) }()
+		go task.Runner(iTask, ctx, false)
 	}
+	// Listen for shutdown signals
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	ctx.Done()
+	fmt.Println("All tasks shutting down completed")
 }
