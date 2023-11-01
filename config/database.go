442b6570f1766bbcf1624849ae0dc96c5982e88d
Daewu <daewu.bintara1996@gmail.com>
2023-11-01 21:20:34 +0700

    add db connection configuration

--- config/database.go
+++ config/database.go
@@ -9,28 +9,34 @@ var db database
 var dbInstance bool = false
 
 type database struct {
-	Name         string
-	Username     string
-	Password     string
-	Host         string
-	Port         string
-	Connection   string
-	UseTimestamp string
-	TimeZone     string
-	Replicas     []database
+	Name                  string
+	Username              string
+	Password              string
+	Host                  string
+	Port                  string
+	Connection            string
+	UseTimestamp          string
+	TimeZone              string
+	Replicas              []database
+	MaxIdleConnection     int
+	MaxIdleConnectionTime int // in second
+	MaxConnMaxLifetime    int // in second
 }
 
 func Database() database {
 	if dbInstance == false {
 		db = database{
-			Name:         os.Getenv("DB_NAME"),
-			Username:     os.Getenv("DB_USERNAME"),
-			Password:     os.Getenv("DB_PASSWORD"),
-			Host:         os.Getenv("DB_HOST"),
-			Port:         os.Getenv("DB_PORT"),
-			UseTimestamp: os.Getenv("DB_USE_TIMESTAMP"),
-			Connection:   os.Getenv("DB_CONNECTION"),
-			TimeZone:     os.Getenv("TIME_ZONE"),
+			Name:                  os.Getenv("DB_NAME"),
+			Username:              os.Getenv("DB_USERNAME"),
+			Password:              os.Getenv("DB_PASSWORD"),
+			Host:                  os.Getenv("DB_HOST"),
+			Port:                  os.Getenv("DB_PORT"),
+			UseTimestamp:          os.Getenv("DB_USE_TIMESTAMP"),
+			Connection:            os.Getenv("DB_CONNECTION"),
+			TimeZone:              os.Getenv("TIME_ZONE"),
+			MaxIdleConnection:     helper.StringToInt(os.Getenv("DB_MAX_IDLE_CONNECTION")),
+			MaxIdleConnectionTime: helper.StringToInt(os.Getenv("DB_MAX_IDLE_CONNECTION_TIME")),
+			MaxConnMaxLifetime:    helper.StringToInt(os.Getenv("DB_MAX_CONN_MAX_LIFETIME")),
 		}
 		if helper.IsExistAllEnvKeys("DB_R1_HOST", "DB_R1_PORT", "DB_R1_NAME", "DB_R1_USERNAME", "DB_R1_PASSWORD").Status == true {
 			newDB := generateReplica(db, "DB_R1_HOST", "DB_R1_PORT", "DB_R1_NAME", "DB_R1_USERNAME", "DB_R1_PASSWORD")
--- pkg/database/connect_to_mysql.go
+++ pkg/database/connect_to_mysql.go
@@ -7,10 +7,18 @@ import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"strings"
+	"time"
 )
 
+// MySqlConfig configuration of mysql connection
+type MySqlConfig struct {
+	MaxIdleConnection     int
+	MaxIdleConnectionTime time.Duration
+	MaxConnMaxLifetime    time.Duration
+}
+
 // connectToMySql : Simplify method and reusable method to connect with mysql driver
-func connectToMySql(username string, password string, host string, port string, name string, useTimeStamp string) *gorm.DB {
+func connectToMySql(username string, password string, host string, port string, name string, useTimeStamp string, config MySqlConfig) *gorm.DB {
 	dsn := username + ":" + password + "@tcp(" + host + ":" + port + ")/" + name + "?parseTime=" + strings.ToLower(useTimeStamp) + "&loc=Local"
 	mainDsn = dsn
 	sqlDB, err1 := sql.Open(MYSQL, dsn)
@@ -18,6 +26,10 @@ func connectToMySql(username string, password string, host string, port string, 
 	if err1 != nil {
 		log.Fatal("Fail open connection to mysql ", err1.Error())
 	}
+
+	sqlDB.SetMaxIdleConns(config.MaxIdleConnection)
+	sqlDB.SetConnMaxIdleTime(config.MaxIdleConnectionTime)
+	sqlDB.SetConnMaxLifetime(config.MaxConnMaxLifetime)
 
 	gormDB, err2 := gorm.Open(mysql.New(mysql.Config{
 		Conn:                     sqlDB,
@@ -39,7 +51,14 @@ func mysqlOpenConnection() *dbc {
 
 	var db = config.Database()
 
-	gormDB := connectToMySql(db.Username, db.Password, db.Host, db.Port, db.Name, db.UseTimestamp)
+	// set mysql configuration by .env
+	dbConf := MySqlConfig{
+		MaxIdleConnection:     db.MaxIdleConnection,
+		MaxIdleConnectionTime: time.Duration(db.MaxIdleConnectionTime) * time.Second,
+		MaxConnMaxLifetime:    time.Duration(db.MaxConnMaxLifetime) * time.Second,
+	}
+
+	gormDB := connectToMySql(db.Username, db.Password, db.Host, db.Port, db.Name, db.UseTimestamp, dbConf)
 
 	var replicas []*gorm.DB
 
@@ -47,7 +66,7 @@ func mysqlOpenConnection() *dbc {
 	if len(db.Replicas) > 0 {
 		for _, replica := range db.Replicas {
 			if replica.Connection == MYSQL {
-				replicaConnect := connectToMySql(replica.Username, replica.Password, replica.Host, replica.Port, replica.Name, replica.UseTimestamp)
+				replicaConnect := connectToMySql(replica.Username, replica.Password, replica.Host, replica.Port, replica.Name, replica.UseTimestamp, dbConf)
 				replicas = append(replicas, replicaConnect)
 			}
 		}
